90dc48579673aef548427986c615bc26e657270d
Elio <Elio@elio>
2022-04-06 18:50:47 -0300

    prueba

---
+++ demo2/src/test/java/ar/edu/unju/fi/pvisual/demo2/aplicacion/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.demo2.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
